162a31934f3d6bd88d24a7b8096e80c1efed4270
Jan FLORENT <jan.florent@mythalesgroup.io>
2018-10-22 17:44:50 +0200

    PAYLAPMEXT-19

--- src/main/java/com/payline/payment/paysafecard/bean/PaySafeRequest.java
+++ src/main/java/com/payline/payment/paysafecard/bean/PaySafeRequest.java
@@ -1,6 +1,5 @@
 package com.payline.payment.paysafecard.bean;
 
-import com.google.gson.annotations.Expose;
 import com.payline.payment.paysafecard.utils.InvalidRequestException;
 import com.payline.payment.paysafecard.utils.PaySafeCardConstants;
 import com.payline.pmapi.bean.payment.ContractConfiguration;
@@ -8,14 +7,13 @@ import com.payline.pmapi.bean.payment.ContractConfiguration;
 import java.util.Base64;
 
 public abstract class PaySafeRequest {
-    @Expose(serialize = false, deserialize = false)
-    private String authenticationHeader;
+    private transient String authenticationHeader;
 
     PaySafeRequest(ContractConfiguration configuration) throws InvalidRequestException {
-        if (configuration == null || configuration.getProperty(PaySafeCardConstants.AUTHORISATIONKEY_KEY).getValue() == null){
+        if (configuration == null || configuration.getProperty(PaySafeCardConstants.AUTHORISATIONKEY_KEY).getValue() == null) {
             throw new InvalidRequestException("PaySafeRequest must have an authorisation key when created");
         } else {
-            this.authenticationHeader = "Basic " + encodeToBase64( configuration.getProperty(PaySafeCardConstants.AUTHORISATIONKEY_KEY).getValue());
+            this.authenticationHeader = "Basic " + encodeToBase64(configuration.getProperty(PaySafeCardConstants.AUTHORISATIONKEY_KEY).getValue());
         }
     }
 
@@ -23,7 +21,7 @@ public abstract class PaySafeRequest {
         return authenticationHeader;
     }
 
-    public static String encodeToBase64(String toEncode){
+    public static String encodeToBase64(String toEncode) {
         if (toEncode == null) toEncode = "";
         return Base64.getEncoder().encodeToString(toEncode.getBytes());
     }
